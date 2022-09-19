aeb30a552b6ae7603b8df7dc74a3e04fd329fbcd
paprikaiec <paprikaiec@gmail.com>
2022-09-19 10:48:34 +0000

    first commit

---
+++ envs/smart_grid/smart_grid_env.py
@@ -0,0 +1,387 @@
+import os
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+from numpy.linalg import matrix_power
+
+from ..multiagentenv import MultiAgentEnv
+import pandapower as pp
+from pandapower import runpp
+import pandapower.networks as networks
+
+from .energy_models import Building, Weather
+
+class GridEnv(MultiAgentEnv):
+    def __init__(self, model_name, data_path, climate_zone, buildings_states_actions_file, hourly_timesteps,
+                 houses_per_node=1, cluster_adjacent_bus_num=6,
+                 save_memory=True, building_ids=None, nclusters=2, randomseed=2, max_num_houses=None, percent_rl=1):
+        self.model_name = model_name
+        if not os.path.isdir(f'model_save/{self.model_name}'):
+            os.mkdir(f'model_save/{self.model_name}')
+        self.max_num_houses = max_num_houses
+#        self.nclusters = nclusters
+        self.percent_rl = percent_rl
+
+        self.cluster_adjacent_bus_num = cluster_adjacent_bus_num
+
+        self.data_path = data_path
+        self.climate_zone = climate_zone
+        self.weather_file = os.path.join(self.data_path, "weather_data.csv")
+        self.solar_file = os.path.join(self.data_path, "solar_generation_1kW.csv")
+        self.weather = Weather(self.weather_file, self.solar_file, hourly_timesteps)
+        self.buildings_states_actions_file = buildings_states_actions_file
+        self.hourly_timesteps = hourly_timesteps
+        self.save_memory = save_memory
+        self.building_ids = building_ids
+
+        self.net = self._make_grid()
+
+        self.buildings = self._add_houses(houses_per_node, 1)  # standard 6 buildings
+        self.agents = list(self.buildings.keys())
+        self.possible_agents = self.agents[:]
+        self.rl_agents = self._set_rl_agents()
+
+        self.clusters = self._get_bus_clusters()
+
+        # self.clusters = self._set_clusters()
+        # self.rl_agents = [x[0] for x in self.clusters]
+        # self.rl_agents = [j for i in self.rl_agents for j in i]
+        # self.ncluster = 0
+
+        self.observation_spaces = {k: v.observation_space for k, v in self.buildings.items()}
+        self.action_spaces = {k: v.action_space for k, v in self.buildings.items()}
+
+        self.metadata = {'render.modes': [], 'name': "gridlearn"}
+
+
+        self.voltage_data = []
+        self.load_data = []
+        self.gen_data = []
+        self.reward_data = []
+        self.all_rewards = []
+
+        self.aspace, self.ospace = self._get_spaces(self.agents)
+        self.single_agent_obs_size = self.ospace[self.agents[0]].shape[0]
+        self.obs_size = self._get_partial_obs_max_len()
+        self.state_size = self.single_agent_obs_size * len(self.agents)
+
+        # self.reset()
+        # rand_act = {k: v.sample() for k, v in aspace.items()}
+        # self.step(rand_act)
+
+        self.v_upper = 1.05
+        self.v_lower = 0.95
+
+
+    def reset(self, reset_logs=True):# TODO: to be tested
+        # self.system_losses = []
+        # self.voltage_dev = []
+        # return {k:self.buildings[k].reset_timestep(self.net, reset_logs) for k in agents}
+        rand_act = {k: v.sample() for k, v in self.aspace.items()}
+        rand_act_array = np.array(list(rand_act.values()))
+        self.steps = 0
+        self.ts = self._select_timestamp()
+        year_ts = self.ts % (8760*self.hourly_timesteps) # TODO:why 96? modified to 4, to be confirmed
+        if year_ts > 90*96 and year_ts < 275*96:
+            self.net.shunt.at[0,'q_mvar'] = -1.8
+            self.net.shunt.at[1,'q_mvar'] = -0.6
+            self.net.shunt.at[2,'q_mvar'] = -1.2
+        else:
+            self.net.shunt.at[0,'q_mvar'] = -1.2
+            self.net.shunt.at[1,'q_mvar'] = -0.01
+            self.net.shunt.at[2,'q_mvar'] = -0.01
+
+        count = 0
+        for agent in self.agents:
+            self.buildings[agent].step(rand_act_array[count])
+            count += 1
+
+        self.ts += 1
+        self.steps += 1
+
+        # update the grid based on updated buildings
+        self._update_grid()
+
+        # run the grid power flow
+        try:
+            runpp(self.net, enforce_q_lims=True)
+            # print(self.net.res_bus.p_mw.tolist())
+        except:
+            pp.diagnostic(self.net)
+            quit()
+            print("QUITTING!!!!")
+
+        for agent in self.agents:
+            self.buildings[agent].reset_timestep(self.net, reset_logs)
+
+        return self.get_obs(), self.get_state()
+
+    def step(self, actions):
+        year_ts = self.ts % (8760*self.hourly_timesteps) # TODO:why 96? modified to 4, to be confirmed
+        if year_ts > 90*96 and year_ts < 275*96:
+            self.net.shunt.at[0,'q_mvar'] = -1.8
+            self.net.shunt.at[1,'q_mvar'] = -0.6
+            self.net.shunt.at[2,'q_mvar'] = -1.2
+        else:
+            self.net.shunt.at[0,'q_mvar'] = -1.2
+            self.net.shunt.at[1,'q_mvar'] = -0.01
+            self.net.shunt.at[2,'q_mvar'] = -0.01
+
+        count = 0
+        for agent in self.agents:
+            self.buildings[agent].step(actions[count])
+            count += 1
+
+        self.ts += 1
+        self.steps += 1
+
+        # update the grid based on updated buildings
+        self._update_grid()
+
+        # run the grid power flow
+        try:
+            runpp(self.net, enforce_q_lims=True)
+            # print(self.net.res_bus.p_mw.tolist())
+        except:
+            pp.diagnostic(self.net)
+            quit()
+            print("QUITTING!!!!")
+        rl_agent_keys = self.agents
+#        obs = self.state(rl_agent_keys)
+        self.voltage_data += [list(self.net.res_bus['vm_pu'])]
+
+        self.load_data += [sum(list(self.net.load['p_mw']))]
+        self.gen_data += [sum(list(self.net.sgen['p_mw']))]
+        return self._get_reward(), self._get_done(), self._get_info()
+
+    def get_state(self): # TODO: to be tested
+        # states = {k: np.array(self.buildings[k].get_state(self.net)) for k in agents}
+        # return states
+        state = np.concatenate([np.array(self.buildings[k].get_state(self.net)) for k in self.agents])
+        return state
+
+    def get_obs(self): # TODO: to be tested
+        all_state_dict = {k: np.array(self.buildings[k].get_state(self.net)) for k in self.agents}
+        pad_obs_list = []
+        for agent in self.rl_agents:
+            agent_obs_array = np.concatenate([all_state_dict[neighbor] for neighbor in self.clusters[agent]])
+            pad_obs_list.append(np.concatenate([agent_obs_array, np.zeros(self.obs_size-agent_obs_array.shape[0])]))
+        return np.array(pad_obs_list)
+
+    def get_obs_agent(self, agent_id):
+        """return observation for agent_id
+        """
+        agents_obs = self.get_obs()
+        return agents_obs[agent_id]
+
+    def get_obs_size(self):
+        """return the observation size
+        """
+        return self.obs_size
+
+    def get_state_size(self):
+        """return the state size
+        """
+        return self.state_size
+
+    def get_num_of_agents(self):
+        return len(self.agents)
+
+    def get_total_actions(self):
+        return self.aspace[self.agents[0]].shape[0]
+
+    def get_avail_actions(self):
+        return [4]
+
+    def _select_timestamp(self):
+        data_len = len(self.weather.data['t_out'])
+        margin = 96
+        return np.random.choice(data_len-margin)
+
+    def _get_spaces(self, agents):
+        # print([self.buildings[k].action_space for k in agents])
+        actionspace = {k: self.buildings[k].action_space for k in agents}
+        obsspace = {k: self.buildings[k].observation_space for k in agents}
+        return actionspace, obsspace
+
+    def _set_rl_agents(self):
+        num_rl_agents = int(self.percent_rl * len(self.net.load.name))
+        rl_agents = np.random.choice(self.net.load.name, num_rl_agents).tolist()
+        return rl_agents
+
+    def _make_grid(self):
+        # make a grid that fits the buildings generated for CityLearn
+        net = networks.case33bw()
+
+        # clear the grid of old load values
+        load_nodes = net.load['bus']
+        res_voltage_nodes = net.bus['name'][net.bus['vn_kv'] == 12.66]
+        res_load_nodes = set(load_nodes) & set(res_voltage_nodes)
+        net.bus['min_vm_pu'] = 0.7
+        net.bus['max_vm_pu'] = 1.3
+
+        for node in res_load_nodes:
+            # remove the existing arbitrary load
+            net.load.drop(net.load[net.load.bus == node].index, inplace=True)
+
+        conns = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                [18, 19, 20, 21],
+                [25, 26, 27, 28, 29, 30, 31, 32],
+                [22, 23, 24]]
+
+        self.pv_buses = [item[-1] for item in conns]
+        self.pv_buses += [item[-2] for item in conns]
+
+        mapping = {18:1, 25:5, 22:2}
+
+        net.line.drop(index=net.line[net.line.in_service==False].index, inplace=True)
+        net.bus_geodata.at[0,'x'] = 0
+        net.bus_geodata.at[0,'y'] = 0
+        sw = 'x'
+        st = 'y'
+        z = -1
+        for c in conns:
+            z += 1
+            for i in range(len(c)):
+                if i == 0:
+                    if not c[i] == 0:
+                        sw = 'y'
+                        st = 'x'
+                        net.bus_geodata.at[c[i], sw] = net.bus_geodata.at[mapping[c[i]],sw] + 0.2
+                        net.bus_geodata.at[c[i], st] = net.bus_geodata.at[mapping[c[i]],st]
+                else:
+                    net.bus_geodata.at[c[i], sw] = net.bus_geodata.at[c[i-1], sw] + 0.2
+                    net.bus_geodata.at[c[i], st] = net.bus_geodata.at[c[i-1], st]
+
+        net.ext_grid.at[0,'vm_pu'] = 1.01
+
+        pp.create_shunt_as_capacitor(net,14,1.2,0)
+        pp.create_shunt_as_capacitor(net,24,0.6,0)
+        pp.create_shunt_as_capacitor(net,30,1.2,0)
+        return net
+
+    def _add_houses(self, n, pv_penetration):
+        if self.max_num_houses:
+            m = 1
+        else:
+            m = n# + np.random.randint(-2,8)
+        houses = []
+        b = 0
+        scaling_number = 6/n
+
+        # find nodes in the network with residential voltage levels and load infrastructure
+        # get the node indexes by their assigned names
+        # load_nodes = self.net.load['bus']
+        ext_grid_nodes = set(self.net.ext_grid['bus'])
+        res_voltage_nodes = set(self.net.bus['name'][self.net.bus['vn_kv'] == 12.66])
+        res_load_nodes = res_voltage_nodes - ext_grid_nodes
+
+        buildings = {}
+        for existing_node in list(res_load_nodes)[:self.max_num_houses]:
+            # remove the existing arbitrary load
+            self.net.load.drop(self.net.load[self.net.load.bus == existing_node].index, inplace=True)
+
+            # add n houses at each of these nodes
+            BuildingId = 0
+            for i in range(m):
+                # bid = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+                BuildingId += 1
+                if not self.building_ids:
+                    with open(self.buildings_states_actions_file) as file:
+                        buildings_states_actions = json.load(file)
+                    self.building_ids = list(buildings_states_actions.keys())
+                    # print(self.building_ids)
+                prob = np.ones(len(self.building_ids))
+                prob[[1,4,5,6,7,8]] = 10 # building at index 0, 2, 8 correspond to buildings with high energy use
+                prob = prob / sum(prob)
+                uid = np.random.choice(self.building_ids, p=prob)
+                # print(uid)
+                bldg = Building(self.data_path, self.climate_zone, self.buildings_states_actions_file, self.hourly_timesteps,
+                                uid, self.weather, BuildingId, save_memory=self.save_memory)
+                bldg.assign_bus(existing_node)
+                bldg.load_index = pp.create_load(self.net, bldg.bus, 0, name=bldg.buildingId, scaling=scaling_number) # create a load at the existing bus
+                if np.random.uniform() <= 2: # equivalent to 100% PV penetration
+                    bldg.gen_index = pp.create_sgen(self.net, bldg.bus, 0, name=bldg.buildingId, scaling=scaling_number) # create a generator at the existing bus
+                else:
+                    bldg.gen_index = -1
+
+                buildings[bldg.buildingId] = bldg
+                # bldg.assign_neighbors(self.net)
+
+        from collections import Counter
+        types = [v.building_type for v in buildings.values()]
+        print(Counter(types))
+
+        return buildings
+
+    def _update_grid(self):
+        for agent, bldg in self.buildings.items():
+            # Assign the load in MW (from KW in CityLearn)
+            self.net.load.at[bldg.load_index, 'p_mw'] = 0.95 * bldg.current_gross_electricity_demand * 0.001
+            self.net.load.at[bldg.load_index, 'sn_mva'] = bldg.current_gross_electricity_demand * 0.001
+
+            if bldg.gen_index > -1:  # assume PV and battery are both behind the inverter
+                self.net.sgen.at[bldg.gen_index, 'p_mw'] = -1 * bldg.current_gross_generation * np.cos(bldg.phi) * 0.001
+                self.net.sgen.at[bldg.gen_index, 'q_mvar'] = bldg.current_gross_generation * np.sin(bldg.phi) * 0.001
+
+    def _get_reward(self):
+        rewards = {k: self.buildings[k].get_reward(self.net) for k in self.agents}
+
+        self.reward_data += [sum(rewards.values())]
+        # self.all_rewards += [rewards.values()]
+        # print(agents)
+        return list(rewards.values())
+
+    def _get_done(self):
+        # dones = {k: (self.buildings[k].time_step >= self.hourly_timesteps*8760) for k in agents}
+        return self.buildings[self.agents[0]].time_step >= self.hourly_timesteps*8760
+
+    def _get_info(self, info={}):
+        v = self.net.res_bus['vm_pu'].sort_index().to_numpy()
+
+        # percentage of voltage out of control
+        percent_of_v_out_of_control = (np.sum(v < self.v_lower) + np.sum(v > self.v_upper)) / v.shape[0]
+        info["percentage_of_v_out_of_control"] = percent_of_v_out_of_control
+
+        # voltage violtation
+        v_ref = 0.5 * (self.v_lower + self.v_upper)
+        info["average_voltage_deviation"] = np.mean(np.abs(v - v_ref))
+        info["average_voltage"] = np.mean(v)
+
+        # line loss
+        line_loss = np.sum(self.net.res_line["pl_mw"])
+        avg_line_loss = np.mean(self.net.res_line["pl_mw"])
+        info["total_line_loss"] = line_loss
+
+        # reactive power (q) loss
+        q = self.net.res_sgen["q_mvar"].sort_index().to_numpy(copy=True)
+        q_loss = np.mean(np.abs(q))
+        info["q_loss"] = q_loss
+
+        return info
+
+    def _get_bus_clusters(self):
+        # calc temp matrix that show adjacent bus
+        G = np.eye(len(self.net.bus))
+        id1 = self.net.line['from_bus'].tolist()
+        id2 = self.net.line['to_bus'].tolist()
+        G[id1, id2] = 1
+        G[id2, id1] = 1
+        temp = matrix_power(G, self.cluster_adjacent_bus_num)
+
+        # clusters: BuildingsID --> adjacent bus BuildingsID
+        clusters = dict()
+        for agent in self.net.load['name'].tolist():
+            agent_bus = self.net.load.loc[self.net.load['name']==agent, 'bus']
+            adjacent_bus = np.where(temp[agent_bus].squeeze()>0)[0]
+            clusters[agent] = self.net.load.loc[self.net.load['bus'].isin(adjacent_bus), 'name'].tolist()
+
+        return clusters
+
+    def _get_partial_obs_max_len(self):
+        max_adjacent_agents = 0
+        for adjacent_list in list(self.clusters.values()):
+            if len(adjacent_list) > max_adjacent_agents:
+                max_adjacent_agents = len(adjacent_list)
+
+        return max_adjacent_agents * self.single_agent_obs_size
---
+++ train.py
@@ -0,0 +1,123 @@
+import argparse
+import multiprocessing
+import sys
+from pathlib import Path
+import gym
+import numpy as np
+import random
+import time
+import yaml
+import wandb
+import torch
+
+from envs.smart_grid.smart_grid_env import GridEnv
+from models.model_registry import Model, Strategy
+from utilities.util import convert, dict2str
+from utilities.trainer import PGTrainer
+
+from tensorboardX import SummaryWriter
+
+MAX_ENV_STEPS=4*4*8759
+
+# tic = time.time()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('alg', type=str, nargs="?", default="ippo")
+argv = parser.parse_args()
+
+#env config
+climate_zone = 1
+data_path = Path("./envs/data/Climate_Zone_"+str(climate_zone))
+buildings_states_actions = './envs/data/buildings_state_action_space.json'
+env_config_dict = {
+    "model_name":"1024agents",
+    "data_path":data_path,
+    "climate_zone":climate_zone,
+    "buildings_states_actions_file":buildings_states_actions,
+    "hourly_timesteps":4,
+    "max_num_houses":None
+}
+env = GridEnv(**env_config_dict)
+
+#alg config
+with open("./args/default.yaml", "r") as f:
+	default_config_dict = yaml.safe_load(f)
+with open("./args/alg_args/" + argv.alg + ".yaml", "r") as f:
+	alg_config_dict = yaml.safe_load(f)["alg_args"]
+	alg_config_dict["action_scale"] = 1.0
+	alg_config_dict["action_bias"] = 0.0
+alg_config_dict = {**default_config_dict, **alg_config_dict}
+alg_config_dict["agent_num"] = env.get_num_of_agents()
+alg_config_dict["obs_size"] = env.get_obs_size()
+alg_config_dict["action_dim"] = env.get_total_actions()
+args = convert(alg_config_dict)
+
+# use wandb
+run = wandb.init(project="grid",
+				 entity="wangyiwen",
+				 config={**env_config_dict, **alg_config_dict},
+				 name=f"{argv.alg}" + f"{env.get_num_of_agents()}")
+
+# create the logger(wandb)
+logger = wandb
+# logger = SummaryWriter("./model_save/" + "tensorboard/" + f"{argv.alg}")
+
+model = Model[argv.alg]
+print(f"{args}\n")
+train = PGTrainer(args, model, env, logger)
+
+for i in range(args.train_episodes_num):
+	stat = {}
+	train.run(stat, i)
+	train.logging(stat)
+	if i % args.save_model_freq == args.save_model_freq - 1:
+		train.print_info(stat)
+		torch.save({"model_state_dict": train.behaviour_net.state_dict()},
+				"./model_save/" + f"{argv.alg}" + f"{env.get_num_of_agents()}" + "/model.pt")
+		print("The model is saved!\n")
+
+# logger.close()
+run.finish()
+
+
+# grid = GridLearn(**config)
+#
+# env = MyEnv(grid) #for _ in range(config['nclusters'])
+# env.grid = grid
+# env.initialize_rbc_agents()
+#
+# print("---env.grid.clusters---")
+# print(env.grid.clusters)
+#
+# print("---env.grid.rl_agent---")
+# print(env.grid.rl_agents)
+
+
+# print('creating pettingzoo env...')
+# env = ss.pettingzoo_env_to_vec_env_v0(env)
+#
+# print('stacking vec env...')
+# # nenvs = 2
+# env = ss.concat_vec_envs_v0(env, 1, num_cpus=1, base_class='stable_baselines3')
+#
+# grid.normalize_reward()
+#
+# model = PPO(MlpPolicy, env, ent_coef=0.1, learning_rate=0.001, n_epochs=30, verbose=2)
+#
+# # nloops=1
+# # for loop in range(nloops):
+# env.reset()
+# print('==============')
+# model.learn(4*4*8759)
+# print('==============')
+# if not os.path.exists(f'models/{model_name}'):
+#     os.makedirs(f'models/{model_name}')
+# os.chdir(f'models/{model_name}')
+# # for m in range(len(models)):
+# #     print('saving trained model')
+# model.save(f"model")
+# os.chdir('../..')
+#
+# toc = time.time()
+# print(toc-tic)
+# print('finished')
